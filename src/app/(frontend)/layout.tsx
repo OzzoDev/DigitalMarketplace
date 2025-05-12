ebf478f687b94ad15149a698f94538f84e0a3320
oscar <oscar.burman005@gmail.com>
2025-05-12 19:37:34 +0200

    Added trpc handler

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from 'next/font/google'
 import './styles.css'
 import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
+import Providers from '@/components/Providers'
 
 const inter = Inter({
   variable: '--font-inter',
@@ -33,8 +34,10 @@ export default function RootLayout({
     <html lang="en" className="h-full">
       <body className={cn('relative h-full font-sans antialiased', inter.className)}>
         <main className="relative flex flex-col min-h-screen">
-          <Navbar />
-          <div className="flex-grow flex-1">{children}</div>
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">{children}</div>
+          </Providers>
         </main>
       </body>
     </html>
