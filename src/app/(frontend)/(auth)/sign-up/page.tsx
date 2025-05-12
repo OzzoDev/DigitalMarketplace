2acbe0fb7e698b236ed0091252fea7fe134f3046
oscar <oscar.burman005@gmail.com>
2025-05-12 17:38:32 +0200

    Implemented react hook form with zod for signup form validation

--- src/app/(frontend)/(auth)/sign-up/page.tsx
+++ src/app/(frontend)/(auth)/sign-up/page.tsx
@@ -7,8 +7,24 @@ import { cn } from '@/lib/utils'
 import { Label } from '@radix-ui/react-label'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import {
+  TAuthCredentialsValidator,
+  AuthCredentialsValidator,
+} from '@/lib/validators/account-credentials-validator'
 
 const Page = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({ resolver: zodResolver(AuthCredentialsValidator) })
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    //send data to server
+  }
+
   return (
     <>
       <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
@@ -27,19 +43,21 @@ const Page = () => {
           </div>
 
           <div className="grid gap-6">
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-2">
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="email">Email</Label>
                   <Input
-                    className={cn({ 'focus-visible:ring-red-500': true })}
+                    {...register('email')}
+                    className={cn({ 'focus-visible:ring-red-500': errors.email })}
                     placeholder="you@example.com"
                   />
                 </div>
                 <div className="grid gap-1 py-2">
                   <Label htmlFor="password">Password</Label>
                   <Input
-                    className={cn({ 'focus-visible:ring-red-500': true })}
+                    {...register('password')}
+                    className={cn({ 'focus-visible:ring-red-500': errors.password })}
                     placeholder="Password"
                   />
                 </div>
